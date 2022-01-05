beda8153cef4b7112ffafb3f6028ed854fbd9fdf
= <pierreambroise@hotmail.fr>
2022-01-05 15:10:14 +0900

    view all basic Rspec code

--- rspec_course/spec/shared_example_spec.rb
+++ rspec_course/spec/shared_example_spec.rb
@@ -1,13 +1,22 @@
+RSpec.shared_examples 'a ruby object with 3 elements' do
+  it 'returns the number of items'do
+    expect(subject.length).to eq(3)
+  end
+end
+
 RSpec.describe Array do
   subject { [1, 2, 3] }
+  include_examples 'a ruby object with 3 elements'
 end
 
 RSpec.describe String do
   subject { 'abc' }
+  include_examples 'a ruby object with 3 elements'
 end
 
 RSpec.describe Hash do
   subject { { a: 1, b: 2, c: 3 } }
+  include_examples 'a ruby object with 3 elements'
 end
 
 class SausageLink
@@ -17,5 +26,6 @@ class SausageLink
 end
 
 RSpec.describe SausageLink do
-subject { described_class.new }
+  subject { described_class.new }
+  include_examples 'a ruby object with 3 elements'
 end
